773c27ac080b5564eb8f039257c47343d02946c1
kvk745 <kvk745@my.utsa.edu>
2022-02-05 15:22:03 -0600

    chapter 1 completed code

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -10,27 +11,32 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var trueToast:Toast
+    private lateinit var falseToast:Toast
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        
+
         trueButton.setOnClickListener {
-            Toast.makeText(
+            trueToast = Toast.makeText(
                 this,
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT)
-                .show()
+            trueToast.setGravity(Gravity.TOP, 0, 0)
+            trueToast.show()
         }
 
         falseButton.setOnClickListener {
-            Toast.makeText(
+            falseToast = Toast.makeText(
                 this,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT)
-                .show()
+            falseToast.setGravity(Gravity.TOP, 0, 0)
+            falseToast.show()
         }
     }
 }
